fc62fedf9cbd441085b28a4b89e7a2bce5c98f46
AyuKamakin <andrey-kamakin@ya.ru>
2024-04-25 22:00:17 +0300

    Добавлены и отлажены комментарии, рейтинг системы (указываются при добавлении в список возврата из любой точки), их редактирование + редактирование постамата для возврата

--- Dialogs/dialog_return_request.py
+++ Dialogs/dialog_return_request.py
@@ -1,7 +1,10 @@
 import operator
 from aiogram_dialog import Dialog, Window
+from aiogram_dialog.widgets.input import MessageInput
 from aiogram_dialog.widgets.kbd import Button, ScrollingGroup, Select
 from aiogram_dialog.widgets.text import Const, Format
+
+from Getters.create_request_getters import get_rating_stars
 from SG.Return_request_SG import *
 from Getters.return_request_getters import *
 from Dialog_functions.return_request_functions import *
@@ -85,6 +88,12 @@ window_choose_postamat = Window(
 window_show_chosen_request = Window(
     Format('Оборудование: {equipment}'),
     Format('Количество: {number} шт'),
+    Format('Постамат для возврата: {postamat}'),
+    Format('Оценка системы: {rating}⭐'),
+    Format('Комментарий: {comment}'),
+    Button(Const("Изменить постамат"), id="update_postamat", on_click=to_update_postamat),
+    Button(Const("Изменить оценку"), id="update_rating", on_click=to_update_rating),
+    Button(Const("Изменить комментарий"), id="update_comment", on_click=to_update_comment),
     Button(Const("Удалить из списка возврата"), id="del_from_return_basket", on_click=to_del_from_return_basket),
     Button(Const("Вернуться"), id="go_back102", on_click=to_return_basket),
     state=Return_Request_SG.show_chosen_request,
@@ -123,6 +132,78 @@ window_deleted_message = Window(
     getter=get_deleted_return_req_info
 )
 
+window_choose_rating = Window(
+    Const("Поставьте оценку системе"),
+    ScrollingGroup(
+        Select(
+            Format("{item[0]}"),
+            item_id_getter=operator.itemgetter(1),
+            items="numbers_list",
+            id="choosing_rating",
+            on_click=set_rating
+        ),
+        id="choose_rate",
+        width=1,
+        height=6,
+    ),
+    Button(Const("Вернуться"), id="go_back114", on_click=to_choose_postamat),
+    state=Return_Request_SG.choose_rating,
+    getter=get_rating_stars
+)
+
+window_update_rating = Window(
+    Const("Поставьте оценку системе"),
+    ScrollingGroup(
+        Select(
+            Format("{item[0]}"),
+            item_id_getter=operator.itemgetter(1),
+            items="numbers_list",
+            id="choosing_rating",
+            on_click=update_rating
+        ),
+        id="choose_rate",
+        width=1,
+        height=6,
+    ),
+    Button(Const("Вернуться"), id="go_back115", on_click=to_show_chosen_request),
+    state=Return_Request_SG.update_rating,
+    getter=get_rating_stars
+)
+
+window_create_comment = Window(
+    Const('Введите комментарий или нажмите "Продолжить"'),
+    MessageInput(comment_created),
+    Button(Const("Продолжить"), id="go_on_to_added", on_click=to_added),
+    Button(Const("Вернуться"), id="go_back115", on_click=to_choose_rating),
+    state=Return_Request_SG.create_comment,
+)
+
+window_update_comment = Window(
+    Const('Введите комментарий'),
+    MessageInput(comment_updated),
+    Button(Const("Вернуться"), id="go_back118", on_click=to_show_chosen_request),
+    state=Return_Request_SG.update_comment,
+)
+
+window_update_postamat = Window(
+    Const("Доступные постаматы для возврата"),
+    ScrollingGroup(
+        Select(
+            Format("{item[0]}"),
+            item_id_getter=operator.itemgetter(1),
+            items="numbers_list",
+            id="choosing_postamat",
+            on_click=update_postamat_return_dialog
+        ),
+        id="choose_num",
+        width=1,
+        height=6,
+    ),
+    Button(Const("Вернуться"), id="go_back100", on_click=to_show_chosen_request),
+    state=Return_Request_SG.update_postamat,
+    getter=get_numbers_of_postamats
+)
 dialog_return_request = Dialog(window_added_message, window_requests_sent_message, window_send_confirmation,
                                window_show_chosen_request, window_choose_postamat, window_show_in_usage,
-                               window_show_basket, window_start, window_show_chosen_in_usage, window_deleted_message)
+                               window_show_basket, window_start, window_show_chosen_in_usage, window_deleted_message,
+                               window_create_comment, window_choose_rating, window_update_comment, window_update_rating, window_update_postamat)
